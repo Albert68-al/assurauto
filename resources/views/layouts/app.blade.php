8426758ef2d15472ae9bf834f762738825ce7e54
Albert68-al <albertmukamba053@gmail.com>
2025-10-30 13:43:54 +0200

    Ajout du logo AssurAuto dans l'application

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/loading.css') }}" rel="stylesheet">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     @stack('styles')
 </head>
 <body>
